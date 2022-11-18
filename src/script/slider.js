8795d1ce28214ac0d286f52f9f37da8d0ef9cd64
Nazar <nazar_pilipenko@inbox.ru>
2022-11-18 18:54:07 +0200

    add modified

---
+++ src/script/slider.js
@@ -0,0 +1,10 @@
+$('.about__slider-content').slick({
+    dots: true,
+    infinite: false,
+    arrows: true,
+    speed: 300,
+    slidesToShow: 1,
+    variableWidth: true,
+    prevArrow: '<img class="slick-prevAr" src="image/icons/left-arr.png" alt="arrow-bottom">',
+    nextArrow: '<img class="slick-nextAr" src="image/icons/right-arr.png" alt="arrow-bottom">'
+  });
